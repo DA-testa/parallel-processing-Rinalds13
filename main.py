8cae9d144787023aca7de65384c9ccd61fec9fd1
Rinalds Ulmanis 221RDB152 <118060249+Rinalds13@users.noreply.github.com>
2023-03-19 21:43:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 # python3
+import heapq
 
 def parallel_processing(n, m, data):
-    output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    izvade = []
+    brivie = [(1, i) for i in range(n)]
+    heapq.heapify(brivie)
+    beigu_laiks = [1] * m
+    for i in range(m):
+        t_sakuma_laiks, index = heapq.heappop(brivie)
+        darba_ilgums = data[i]
+        sakuma_laiks = max(t_sakuma_laiks, beigu_laiks[i-1])
+        beigu_laiks = sakuma_laiks + darba_ilgums
+        izvade.append((index, sakuma_laiks))
+        beigu_laiks[i] = beigu_laiks
+        heapq.heappush(brivie, (beigu_laiks, index))
+    return izvade
 
-    return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
+    n m = map(int, input().split())
+    dati = list(map(int, input().split()))
+    rezultati = parallel_processing(n, m, dati)
+    for index, sakuma_laiks in rezultati:
+        print(index, sakuma_laiks)
 
 if __name__ == "__main__":
     main()
